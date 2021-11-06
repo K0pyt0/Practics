f0cae32c9f5abd4a91f83e576fd81503503e0a65
K0pyt0 <68741561+K0pyt0@users.noreply.github.com>
2021-11-06 17:11:59 +0300

    Task №4 3rd var solved

---
+++ Practics53/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practics53
+{
+    class Program
+    {
+        static int find_del_sum(int N)
+        {
+            int sum = 0;
+            for (int i = 1; i < N / 2 + 1; i++)
+            {
+                if (N % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i ++)
+            {
+                if (find_del_sum(i) == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
