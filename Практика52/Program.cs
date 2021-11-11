6ad110a94dca000766ac7f8efff57a3a199473bf
K0pyt0 <ivan@alick.ru>
2021-11-11 14:47:56 +0300

    Practice3 solved
    In case of showing this code, tell the credentials

--- Практика52/Program.cs
+++ Практика52/Program.cs
@@ -4,50 +4,56 @@ namespace Практика52
 {
     class Program
     {
-        static void Main(string[] args)
+        static string less8andOn(int N)
         {
-            static string N1(int N)
+            int count = 0;
+            int tmp = 0;
+            while (N > 0)
             {
-                int count = 0;
-                int tmp = 0;
-                while (N > 0)
+                tmp = N % 10;
+                if (tmp < 8)
                 {
-                    tmp = N % 10;
-                    if (tmp < 8 && tmp % 3 != 0 && tmp % 4 != 0) count++;
-                    N = N / 10;
+                    if (tmp % 3 != 0)
+                    {
+                        if (tmp % 4 != 0) count++;
+                    }
                 }
-
-                if (count == 0) return "NO";
-                else return Convert.ToString(count);
+                N /= 10;
             }
 
-            static int N2(int N)
+            if (count == 0) return "NO";
+            else return Convert.ToString(count);
+        }
+
+        static int EvenPairsSum(int N)
+        {
+            int tmp1 = 0;
+            int tmp2 = 0;
+            int sum = 0;
+            while (N > 0)
             {
-                int tmp1 = 0;
-                int tmp2 = 0;
-                int sum = 0;
-                while (N > 0)
-                {
-                    tmp1 = N % 10;
-                    tmp2 = (N / 10) % 10;
-                    if ((tmp1 + tmp2) % 2 == 0) sum += tmp1 + tmp2;
-                }
-                return sum;
+                tmp1 = N % 10;
+                tmp2 = (N / 10) % 10;
+                if ((tmp1 + tmp2) % 2 == 0) sum += tmp1 + tmp2;
+                N /= 10;
+            }
+            return sum;
+        }
+
+        static string HappyNumber(int N)
+        {
+            int tmpN = N;
+            int count = 0;
+            while (tmpN > 0)
+            {
+                count++;
+                tmpN = tmpN / 10;
             }
 
-            static bool N3(int N)
-            {
-                int tmpN = N;
-                int count = 0;
-                while (tmpN > 0)
-                {
-                    count++;
-                    tmpN = tmpN / 10;
-                }
-
-                int sum1 = 0;
-                int sum2 = 0;
-                int i = 0;
+            int sum1 = 0;
+            int sum2 = 0;
+            int i = 0;
+            if (count % 2 == 0){
                 while (i < count / 2)
                 {
                     sum1 += N % 10;
@@ -59,19 +65,37 @@ namespace Практика52
                     sum2 += N % 10;
                     N = N / 10;
                 }
-
-                if (sum1 == sum2) return true;
-                else return false;
             }
-
-            static void Main(string[] args)
+            else
             {
-                int N = int.Parse(Console.ReadLine());
-                Console.WriteLine($"{N}");
-                Console.WriteLine(N1(N));
-                Console.WriteLine($"{N2(N)}");
-                Console.WriteLine($"{N3(N)}");
+                while (i < count / 2)
+                {
+                    sum1 += N % 10;
+                    N = N / 10;
+                    i++;
+                }
+                sum1 += N % 10;
+                while (N > 0)
+                {
+                    sum2 += N % 10;
+                    N = N / 10;
+                }
             }
+
+            if (sum1 == sum2) return "счастливое";
+            else return "несчаcтливое";
+        }
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string ans1 = less8andOn(N);
+            int ans2 = EvenPairsSum(N);
+            string ans3 = HappyNumber(N);
+
+            Console.WriteLine($"{ans1}");
+            Console.WriteLine($"{ans2}");
+            Console.WriteLine($"Ваше число {ans3}");
         }
     }
 }
