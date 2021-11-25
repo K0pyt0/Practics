14ec1913b3ce19a6db6c761613aa3a9bec066093
K0pyt0 <ivan@alick.ru>
2021-11-25 15:35:50 +0300

    Gnomes beginned, N7 solved

---
+++ Гномы/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Гномы
+{
+    class MainClass
+    {
+        static uint InputGN()
+        {
+            Console.WriteLine("Введите количество гномов (16 или 32):");
+            uint GN = uint.Parse(Console.ReadLine());
+            if (GN == 16 || GN == 32) return GN;
+            else return WrongInputGN();
+        }
+
+        static uint WrongInputGN()
+        {
+            Console.WriteLine("Ошибка: введённое число должно быть равно 16 или 32. Повторите ввод:");
+            uint GN = uint.Parse(Console.ReadLine());
+            if (GN == 16 || GN == 32) return GN;
+            else return WrongInputGN();
+        }
+
+        static ushort InputGPS()
+        {
+            Console.WriteLine("Введите число, двоичный код которого представляет гномов:");
+            ushort GP = ushort.Parse(Console.ReadLine());
+            return GP;
+        }
+
+        static uint InputGPI()
+        {
+            Console.WriteLine("Введите число, двоичный код которого представляет гномов:");
+            uint GP = uint.Parse(Console.ReadLine());
+            return GP;
+        }
+
+        public static void Main(string[] args)
+        {
+            uint GN = InputGN();
+            ushort GPS = InputGPS();
+            uint GPI = InputGPI();
+
+            Console.WriteLine(GN);
+            Console.WriteLine($"{GPS}, {GPI}");
+        }
+    }
+}
---
+++ Практика7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Практика7
+{
+    class MainClass
+    {
+        static void PlusMinus(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b - b;
+
+        }
+
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            PlusMinus(ref a, ref b);
+            Console.WriteLine($"{a}, {b}");
+        }
+    }
+}
