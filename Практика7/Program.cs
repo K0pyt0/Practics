1bfcb97e4e7a3279b08ee06392069680529925a2
K0pyt0 <ivan@alick.ru>
2021-11-27 22:19:58 +0300

    Edited 7.2

--- Практика7/Program.cs
+++ Практика7/Program.cs
@@ -4,18 +4,29 @@ namespace Практика7
 {
     class MainClass
     {
-        static void PlusMinus(ref int a, ref int b)
+        static int Def(int b)
         {
-            a = a + b;
-            b = a - b - b;
+            int count = 0;
+            while (b > 0)
+            {
+                count++;
+                b /= 10;
+            }
+            return count;
+        }
 
+        static void Incr(ref int a, int b)
+        {
+            for (int i = 0; i < b; i++) a *= 10;
         }
 
         public static void Main(string[] args)
         {
+            Console.WriteLine("Ввелите 2 числа");
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
-            PlusMinus(ref a, ref b);
+            b = Def(b);
+            Incr(ref a, b);
             Console.WriteLine($"{a}, {b}");
         }
     }
--- Практика72/Program.cs
+++ Практика72/Program.cs
@@ -4,9 +4,23 @@ namespace Практика72
 {
     class MainClass
     {
+        static void SetBigg(ref int a)
+        {
+            for (int i = a / 2; i > 0; i--)
+            {
+                if ((a % i) == 0)
+                {
+                    a = i;
+                    break;
+                }
+            }
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            SetBigg(ref a);
+            Console.WriteLine(a);
         }
     }
 }
