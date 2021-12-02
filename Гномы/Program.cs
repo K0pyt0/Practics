4f3bc0ec86b7627bc52a16340bebc3f393c89320
K0pyt0 <ivan@alick.ru>
2021-12-02 20:31:46 +0300

    Gnomes updated

--- Гномы/Program.cs
+++ Гномы/Program.cs
@@ -30,9 +30,10 @@ namespace Гномы
         static void Output(uint GP, int GN)
         {
             Console.WriteLine("Гномы:");
-            for (int i = 32; i <= 0; i++)
+            int i = 32 - GN;
+            for (; i < 32; i++)
             {
-                if (((GP << i) & 80000000) == 80000000) Console.Write("1");
+                if (((GP << i) & 0x80000000) == 0x80000000) Console.Write("1");
                 else Console.Write("0");
             }
             Console.WriteLine();
@@ -40,21 +41,29 @@ namespace Гномы
 
         static bool AreForwardEven(uint GP, int GN, int pos)
         {
-            GP >>= pos;
+            GP >>= pos + 1;
             int count = 0;
-            for (int i = 0; i < (GN - pos); i++)
+            for (int i = 1; i < (GN - pos); i++)
             {
                 if (((GP >> i) & 1) == 1) count++;
             }
-            if (count % 2 == 0) return true;
-            else return false;
+            if (count % 2 == 0)
+            {
+                Console.WriteLine("Forward are even");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Forward are uneven");
+                return false;
+            }
         }
         
         static void Say(bool ans, uint GP, int pos, ref int score)
         {
             bool rans = ((GP >> pos) & 1) == 1;
-            if (ans) Console.WriteLine($"{pos + 1}-й гном сказал: \"Я в красной шапке.\"");
-            else Console.WriteLine($"{pos + 1}-й гном сказал: \"Я в зелёной шапке.\"");
+            if (ans) Console.WriteLine($"{pos + 1}-й гном сказал: \"Я в зелёной шапке.\"");
+            else Console.WriteLine($"{pos + 1}-й гном сказал: \"Я в красной шапке.\"");
             if (ans == rans)
             {
                 score++;
@@ -75,6 +84,7 @@ namespace Гномы
             Say(isNumEven, GP, 0, ref score);
             for (int i = 1; i < GN; i++)
             {
+                Console.WriteLine(isNumEven);
                 if (isNumEven == AreForwardEven(GP, GN, i)) Say(false, GP, i, ref score);
                 else
                 {
