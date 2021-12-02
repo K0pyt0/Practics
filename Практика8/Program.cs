584c052d0ed651bba15b5374018d0f6f0a2abc41
K0pyt0 <68741561+K0pyt0@users.noreply.github.com>
2021-12-02 22:24:34 +0300

    It was really hard, yeah

---
+++ Практика8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Практика8
+{
+    class Program
+    {
+        static int F(int n)
+        {
+            if (n > 30) return n * n + 5 * n + 4;
+            else if (n % 2 == 0) return F(n + 1) + 3 * F(n + 4);
+            else return 2 * F(n + 2) + F(n + 5);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(27);
+        }
+    }
+}
