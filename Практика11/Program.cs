e5056217573970be19309fd95da134df997738a0
K0pyt0 <ivan@alick.ru>
2022-01-24 10:00:05 +0300

    Practice 11 created file

---
+++ Практика11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Практика11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
